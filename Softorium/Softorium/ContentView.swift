16f47c8c47fe11f23ac34ea7d5d0f0f12f875041
AlexKotov <alexkotov0408@gmail.com>
2023-12-08 11:27:13 +0100

    created project

---
+++ Softorium/Softorium/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Softorium
+//
+//  Created by AlexKotov on 8.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Softorium/Softorium/SoftoriumApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoftoriumApp.swift
+//  Softorium
+//
+//  Created by AlexKotov on 8.12.23.
+//
+
+import SwiftUI
+
+@main
+struct SoftoriumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
